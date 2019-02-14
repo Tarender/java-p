9145b5acd1576402049b522f52f851ea3d31570e
Tarender <mundi0@op.pl>
2019-02-14 10:05:11 +0100

    Start

---
+++ src/start/Hello.java
@@ -0,0 +1,9 @@
+package start;
+// komentarz
+// psvm - public static void main(string[] args)
+// sout - system.out.println();
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
